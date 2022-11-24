704264ac83bc8554408701c6537b7018b6c3c652
dominion-30 <abiolaayobamidele@gmail.com>
2022-11-24 16:14:23 +0100

    Add file

--- js/main.js
+++ js/main.js
@@ -43,10 +43,12 @@
 
 //ask for help
 
-// let admin, name;
+// let admin;
+// let name;
 // name = "John";
 // admin = name;
 // alert(admin);
+
 
 // //user is a visitor to the site
 
@@ -58,17 +60,39 @@
 // [earth,currentUser].forEach(alert);
 
 
-let name = "Abiola";
 
-let num = 4;
-num = 3.142;
-alert(num / 0);
+// let num = 4;
+// num = 3.142;
+// alert(num / 0);
 
-alert(Infinity);
+// alert(Infinity);
 
-alert("this is not a number"/5);
-alert(5 + 3);
-alert(NaN / 5 - 12);
-alert(NaN * 0);
-alert(NaN ** 0);
+// alert("this is not a number"/5);
+// alert(5 + 3);
+// alert(NaN / 5 - 12);
+// alert(NaN * 0);
+// alert(NaN ** 0);
 
+// const bigInt = 123456789012345678901234567890123456789n;
+// alert(bigInt);
+ 
+
+// let str = "Samuel";
+// let str2 = 'Single quotes are ok too';
+// let phrase = `How are you  ${str}`;
+
+// alert(phrase);
+
+// alert(`the result is ${2**5}`);
+
+
+// let nameFieldChecked = true;
+// let ageFieldChecked = false;
+
+// alert(nameFieldChecked);
+
+// let greater = 4 > 1;
+// alert(`the result of 4 > 1 is ${greater}`);
+
+let age = null;
+alert(`${age}`);
